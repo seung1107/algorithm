93492ad0761c9a094ac2adce635927f845f83176
seung1107 <seung6870@gmail.com>
2016-07-16 23:00:13 +0900

    Add files via upload

--- LinkedListStack.c
+++ LinkedListStack.c
@@ -26,3 +26,34 @@ void Pop(LinkedListStack* stack);
 Node* Top(LinkedListStack* stack);
 int getSize(LinkedListStack* stack);
 int isEmpty(LinkedListStack* stack);
+
+
+
+void createStack(LinkedListStack** stack)
+{
+	(*stack) = (LinkedListStack*)malloc(sizeof(LinkedListStack));
+	(*stack)->List = NULL;
+	(*stack)->Top = NULL;
+}
+
+void destroyStack(LinkedListStack* stack)
+{
+	while(!isEmpty(stack))
+	{
+		Node* Popped = Pop(stack);
+		destroyNode(Popped);
+	}
+	free(stack);
+}
+
+Node* createNode(char* newData)
+{
+	Node* newNode = (Node*)malloc(sizeof(Node));
+	newNode->data = (char*)malloc(strlen(newData)+1);
+	
+	strcpy(newNode->data, newData);
+	
+	newNode->nextNode = NULL;
+	
+	return newNode;
+}
